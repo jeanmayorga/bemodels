129bf7671e614a51c235af95d85c633f03cdc421
Jean Paul Mayorga <jpmayorga@outlook.com>
2022-03-08 16:02:28 -0500

    feat: adding google analitiycs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-63B1NVZZH8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-63B1NVZZH8');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ChakraProvider>
   );
